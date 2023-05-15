95e1e75cfd9bc33388414c236c0c5dbd7032531e
AMANDA FERNANDES CUSTODIO <58089751+amanda-noda@users.noreply.github.com>
2023-05-15 20:49:31 -0300

    update: atualiza App

--- grafos/src/App.js
+++ grafos/src/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom' 
 import './App.css';
+import Home from '../pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { getLocales, getCalendars } from 'expo-localization';
+import React, {useState,useEffect,useRef} from 'react';
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
 function App() {
   return (
     <>
-    <Router>
-      <Navbar />
+     <Router>
+      <h1>Zelda : Ocarina of time</h1>
       <Switch>
-        <Route path='/' exact/>
+        <Route path='/' exact component={Home} />
       </Switch>
     </Router>
+    
+    
     </>
-  );
+  )
 }
 
 export default App;
